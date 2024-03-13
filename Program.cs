6c38c2f2e46f96eec82437edd17a96c70be32511
Md Jubiar Ahmed Rabbi <41544413+jubiarahmed@users.noreply.github.com>
2024-03-13 19:29:25 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("1.TodoList");
+Console.WriteLine("2.Add TodoList");
+Console.WriteLine("3.Remove List");
+Console.WriteLine("4.Exit");
+
+var userchoice = Console.ReadLine();
+
+if (userchoice == "1")
+{
+    PrintSelectedOption("TODOs");
+}
+else if (userchoice == "2")
+{
+    PrintSelectedOption("Add a TODO");
+}
+else if (userchoice == "3")
+{
+    PrintSelectedOption("Remove a TODO");
+}
+else if (userchoice == "4")
+{
+    PrintSelectedOption("Exit");
+}
+
+Console.ReadKey();
+void PrintSelectedOption(string SelectedOption)
+{
+    Console.WriteLine("Selected option: " + SelectedOption);
+}
